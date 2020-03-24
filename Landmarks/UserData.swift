97b92929b30d65b5363d93a50135fc41ee41f593
msarmi9 <msarmiento3@usfca.edu>
2020-03-24 14:19:26 -0700

    complete BuildingListsAndNavigation section

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  USerData.swift
+//  Landmarks
+//
+//  Created by Matthew Sarmiento on 3/24/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
