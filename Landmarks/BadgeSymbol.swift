86ccd90d36f2e96b5a932536ae8edca21f8dcc92
msarmi9 <msarmiento3@usfca.edu>
2020-03-25 09:44:49 -0700

    finished badge

---
+++ Landmarks/BadgeSymbol.swift
@@ -0,0 +1,50 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Matthew Sarmiento on 3/25/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path {path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = 0.75 * width
+                let spacing = 0.03 * width
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing + topHeight / 2),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                
+                path.move(to: CGPoint(x: middle, y: 3 * spacing + topHeight / 2))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: spacing + topHeight),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: 3 * spacing + topHeight / 2)
+                ])
+            }
+            .fill(Self.symbolColor)
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
